b23ede6df521ea83052197886063b24ca4da95c4
amie-affection <amie.affection@gmail.com>
2020-05-22 13:17:07 +0300

    modified homepage.js

--- js/pages/homepage.js
+++ js/pages/homepage.js
@@ -8,33 +8,3 @@ export const renderStartPage = () => {
   trandingList.addEventListener("click", getId);
   getMovies().then((data) => (trandingList.innerHTML = itemsRender(data)));
 };
-
-{
-  // export const  renderStartPage = async () => {
-  //   const container = document.querySelector('.container');
-  //   const trandingFilmsList = filmsArr => `
-  //   <h2 class="tranding-title">Trending today</h2>
-  //     <ul class="tranding-list">
-  //     ${itemsRender(filmsArr)}
-  //     </ul>
-  //     `;
-  //     await getMovies ().then(data => (container.innerHTML = trandingFilmsList(data)));
-  //   const trandingList = document.querySelector(".tranding-list");
-  //   trandingList.addEventListener('click', getId)
-  // };
-  // export const renderStartPage = () => {
-  //   const trandingFilmsList = filmsArr => {
-  //     const container = document.querySelector('.container');
-  //     const markup = `
-  //   <h2 class="tranding-title">Trending today</h2>
-  //     <ul class="tranding-list">
-  //     ${itemsRender(filmsArr)}
-  //     </ul>
-  //     `;
-  //     container.innerHTML = markup;
-  //     const trandingList = document.querySelector('.tranding-list');
-  //     trandingList.addEventListener('click', getId);
-  //   };
-  //   getMovies().then(data => trandingFilmsList(data));
-  // };
-}
